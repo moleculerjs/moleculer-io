88504e1745ad88a6eee2bd4dfe74547b0a0fafda
Andrejus <anba8005@gmail.com>
2019-11-30 14:38:33 +0200

    ts bindings

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare module "moleculer-io" {
+	import { ServiceSchema } from "moleculer";
+	const SocketIOService: ServiceSchema
+	export = SocketIOService;
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+ * moleculer
+ * Copyright (c) 2018 MoleculerJS (https://github.com/moleculerjs/moleculer)
+ * MIT Licensed
+ */
+
+module.exports = require("./src");
